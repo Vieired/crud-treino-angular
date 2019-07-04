c11927ad824882b1e68e8c1993b492fcc3420237
egomesmtt <egomes@metatron.com.br>
2019-07-04 12:37:22 -0300

    Inclusão de telas (Produtos, Clientes e Dashboard)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashbard', component: DashboardComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'produtos', component: ProdutosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent,
+    ClientesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

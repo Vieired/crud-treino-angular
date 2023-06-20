237b20126bb9f5bb070afa6f9b0b2beb38ca3f8e
Ednaldo Gomes <egomes@metatron.com.br>
2023-06-20 16:40:07 -0300

    feat(clientes): adiciona breadcrumb

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent implements OnInit {
-
+  tituloPagina: string = "Clientes";
   dropdownList = [];
   selectedItems = [];
   dropdownSettings = {};

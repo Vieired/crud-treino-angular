6cbdcc6f36555e3f0cbbb99171e1af6435feb130
Ednaldo Gomes <egomes@metatron.com.br>
2023-06-20 15:45:20 -0300

    refactor(produtos): altera título da página e do breadcrumb para usar variável

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+// interface ItensBreadcrumb {
+//   nome: string,
+//   caminho: string;
+// }
+
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.scss']
 })
 export class ProdutosComponent implements OnInit {
+  // public itens: ItensBreadcrumb[] = [
+  //   { nome: 'Dashboard', caminho: '/dashboard'},
+  //   { nome: 'Contato', caminho: '/contato'},
+  // ];
+  private tituloPagina: string = "Produtos"
 
   tiles: any[] = [
     {text: 'Um', cols: 3, rows: 1, color: 'lightblue'},
